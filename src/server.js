23c475e10ecc76e6d626c2266576ae9cdd6c4d2d
Mahmoud Hammad <mahmoudhammad3009@gmail.com>
2022-08-12 22:17:03 +0300

    get PORT from env file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 const http = require('http');
 const router = require('./router');
 
 const server = http.createServer(router);
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log('Server is listening on port 300');
 });
