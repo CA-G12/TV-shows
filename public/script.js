087b23719683d436780bdc70ca465aeda3256859
Mahmoud Hammad <mahmoudhammad3009@gmail.com>
2022-08-12 19:55:48 +0300

    create the movies cards & fix the style & and configration for eslint and add the external fetch functionrelated #22

--- public/script.js
+++ public/script.js
@@ -1,14 +1,28 @@
 const fetch = (endpoint) => {
-  const xhr = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4) {
+    if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        console.log(JSON.parse(xhr.responseText))
-      }else {
-        console.log("ERROR", xhr.status)
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log('ERROR', xhr.status);
       }
     }
-  }
-  xhr.open("GET", `/data?q=${endpoint}`)
-  xhr.send()
-}
+  };
+
+  xhr.open('GET', `/data?q=${endpoint}`);
+  xhr.send();
+};
+
+const externalFetch = (title) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log('ERROR', xhr.status);
+      }
+    }
+  };
+};
